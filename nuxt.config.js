030f096cd744dc40c8fcdae04e705a086bc023a1
Muhaddis <muhaddisshah@gmail.com>
2020-07-06 21:05:34 +0500

    :recycle: Remove color mode module

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,8 +66,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/color-mode'
+    '@nuxtjs/eslint-module'
   ],
   /*
    ** Nuxt.js modules
