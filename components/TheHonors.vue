71db9ab714ca387da96b25f7a9d706d9567dae5f
Muhaddis <muhaddisshah@gmail.com>
2020-07-13 04:12:03 +0500

    :sparkles: Sub Component for Props

--- components/TheHonors.vue
+++ components/TheHonors.vue
@@ -3,74 +3,31 @@
     <div class="in">
       <h2>Achievements</h2>
 
-      <div id="work_wrap">
+      <div>
         <p>
           List of my security findings and acknowledgements
         </p>
 
         <ul>
-          <li
-            id="post-445"
-            class=" post-445 work type-work status-publish format-standard has-post-thumbnail hentry tag-bug-bounty tag-bug-hunting tag-bugcrowd tag-cyber-security tag-hackerone tag-hackerone-reports tag-idor tag-microsoft tag-minecraft tag-msrc tag-security-vulnerability tag-web-security tag-xss works-acknowledgements"
-          >
-            <a
-              class="item"
-              href="https://www.muhaddis.info/work/18th-acknowledgement-from-Microsoft/"
-              ><img
-                src="https://www.muhaddis.info/wp-content/uploads/2018/07/Microsoft-Hall-of-Fame-370x249.png"
-                alt="18th Acknowledgement From Microsoft"
-            /></a>
-          </li>
-
-          <li
-            id="post-397"
-            class=" post-397 work type-work status-publish format-standard has-post-thumbnail hentry tag-account-takeover tag-cyber-security tag-information-system-security tag-whitehat-hacking works-acknowledgements"
-          >
-            <a
-              class="item"
-              href="https://www.muhaddis.info/work/17th-acknowledgement-sony/"
-              ><img
-                src="https://www.muhaddis.info/wp-content/uploads/2017/11/Sony-Hall-of-Thanks-370x395.jpg"
-                alt="17th Acknowledgement From Sony"
-            /></a>
-          </li>
-
-          <li
-            id="post-385"
-            class=" post-385 work type-work status-publish format-standard has-post-thumbnail hentry tag-acknowledgement tag-bug-bounty tag-bug-hunter tag-bug-hunter-google tag-bug-hunter-university tag-cyber-security tag-ethical-hacking tag-google tag-google-vrp tag-hack tag-hack-the-world tag-hacking tag-hacktober tag-hall-of-fame tag-iot tag-newsletter tag-security tag-session-management tag-technology tag-vulnerability tag-vulnerability-reward-program tag-whitehat works-acknowledgements"
-          >
-            <a
-              class="item"
-              href="https://www.muhaddis.info/work/16th-acknowledgement-google/"
-              ><img
-                src="https://www.muhaddis.info/wp-content/uploads/2017/10/Google-VRP-Profile-370x260.jpg"
-                alt="16th Acknowledgement From Google"
-            /></a>
-          </li>
-
-          <li
-            id="post-370"
-            class="item post-370 work type-work status-publish format-standard has-post-thumbnail hentry tag-adobe tag-bug-hunter tag-bug-hunting tag-bugbounty tag-cyber-security tag-dmarc tag-email-spoofing tag-emkei tag-ethical-hacker tag-hacker tag-hackerone tag-penetration-testing tag-responsible-disclosure tag-spf tag-whitehat works-acknowledgements"
-          >
-            <a
-              class="item"
-              href="https://www.muhaddis.info/work/14th-acknowledgement-adobe/ "
-              ><img
-                src="https://www.muhaddis.info/wp-content/uploads/2017/09/Adobes-Hall-of-Fame-370x298.jpg"
-                alt="14th Acknowledgement From Adobe"
-            /></a>
-          </li>
+          <TheProjectItem />
+          <TheProjectItem />
+          <TheProjectItem />
+          <TheProjectItem />
         </ul>
       </div>
     </div>
   </div>
 </template>
-
 <script>
-export default {}
+import TheProjectItem from './TheProjectItem'
+export default {
+  components: {
+    TheProjectItem
+  }
+}
 </script>
 
-<style scoped>
+<style>
 #honors ul {
   column-count: 2;
   column-gap: 5;
@@ -100,6 +57,10 @@ export default {}
 @media only screen and (max-width: 320px) {
   #honors ul li {
     width: 100%;
+  }
+  #honors ul {
+    column-count: 1;
+    column-gap: 0;
   }
 }
 </style>
---
+++ components/TheProjectItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>
+    <a href="https://www.muhaddis.info/work/14th-acknowledgement-adobe/ "
+      ><img
+        src="https://www.muhaddis.info/wp-content/uploads/2017/09/Adobes-Hall-of-Fame-370x298.jpg"
+        alt="14th Acknowledgement From Adobe"
+    /></a>
+  </li>
+</template>
+
+<script>
+export default {}
+</script>
